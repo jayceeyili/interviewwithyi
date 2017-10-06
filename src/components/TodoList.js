6261b691603119033445c826f8563efb4af80187
Li Yi <jayceeyili@gmail.com>
2017-10-06 00:05:33 -0700

    add logo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,15 +2,28 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Todo from './Todo'
 import img from '../todo.jpg'
-import { List, Image } from 'semantic-ui-react'
+import { List, Image, Popup, Icon } from 'semantic-ui-react'
 
 const TodoList = ({ todos, onTodoClick }) => {
   const empty = todos.length === 0;
+  const style = {
+    borderRadius: 30,
+    opacity: 0.7,
+    padding: '0.5em',
+  }
 
   return (
     <div>
       {empty ? (
-        <Image src={img} className='img' />
+        <Popup trigger={<Image src={img} className='img' />}
+        position='top right'
+        inverted
+        style={style}
+        >
+          <Popup.Content>
+            <Icon name='idea' />
+          </Popup.Content>
+        </Popup>
       ) : (
       <List >
         {todos.map(todo =>
